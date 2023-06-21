d970e7ed420854eff7b4f441cbf0cf75783df0b9
Rushil Shah <shah.rushil.s@gmail.com>
2023-06-21 13:57:19 -0400

    made the code functional and made minor simplificaotins

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import serial
 import datetime
-import sys
-import glob
+import time
 
 
-STARTING_LATITUDE = 42.654361
-STARTING_LONGITUDE = -83.23361
-STARTING_ALTITUDE = 282  # meters
+STARTING_LATITUDE = 42.654  # 42.654 is the starting LAT for CTC
+STARTING_LONGITUDE = -83.233  # 83.233 is the starting LON for CTC
+STARTING_ALTITUDE = 282  # (in meters) 282 is the starting ALT for CTC
 
 PORT = ""
-BAUDRATE = 115200
-NAME = ""
-
-
-def get_serial_port_location_by_serial_number(serial_number):
-    for port in serial.tools.list_ports.comports():
-        if port.serial_number == serial_number:
-            return port.location
+BAUD = 115200
+NAME = "Silicon Labs CP210x USB to UART Bridge"
 
 
 def get_serial_port_location_by_name(device_name):
@@ -25,43 +18,21 @@ def get_serial_port_location_by_name(device_name):
             return port.location
 
 
-
-def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-
 def run_locations(filename):
     with (
         open(filename, 'r') as file,
-        serial.Serial(port=PORT, baudrate=BAUDRATE) as ser
+        serial.Serial(port=PORT, baudrate=BAUD) as ser
     ):
-        line = file.readline()
-        ser.write(data=line.encode("UTF-8"))
+        for line in file.readline():
+            line = line.strip()
+            line += "\r"
+            ser.write(data=line.encode("UTF-8"))
+            time.sleep(0.5)
+            bytes_to_read = ser.inWaiting()
+            response = ser.read(bytes_to_read)
+            print(response.decode())
+            time.sleep(0.5)
+            ser.write(data=line.encode("UTF-8"))
 
 
 def main():
@@ -69,15 +40,24 @@ def main():
     This function sets the default time, date, and location for CLAW Simulator GPS
     """
     now = datetime.datetime.now()
+
+    manual_setting_string = "SIM:MODE MANUAL"
     time_setting_string = f"SIMulation:TIME:START:TIME {now.hour},{now.minute},{now.second}"
-    date_setting_string = f"SIMulation:TIME:START:DATE {now.year},{now.month},{now.day}"  # TODO Does this need z-fill
+    date_setting_string = f"SIMulation:TIME:START:DATE {now.year},{now.month},{now.day}"
     position_setting_string = f"SIMulation:POSition:LLH {STARTING_LATITUDE},{STARTING_LONGITUDE},{STARTING_ALTITUDE}"
 
-    # the GPS requires 8-N-1 which is default the baud rate required is 115200
-    # https://en.wikipedia.org/wiki/8-N-1
-    with serial.Serial(port=PORT, baudrate=BAUDRATE) as ser:
-        for string in [time_setting_string, date_setting_string, position_setting_string]:
-            ser.write(data=string.encode("UTF-8"))
+    commands = [manual_setting_string, time_setting_string, date_setting_string, position_setting_string]
+
+    with serial.Serial(port=PORT, baudrate=BAUD) as ser:
+        time.sleep(0.5)
+        for command in commands:
+            command += "\r"
+            ser.write(data=command.encode("UTF-8"))
+            time.sleep(0.5)
+            bytes_to_read = ser.inWaiting()
+            response = ser.read(bytes_to_read)
+            print(response.decode())
+            time.sleep(0.5)
 
 
 if __name__ == '__main__':
